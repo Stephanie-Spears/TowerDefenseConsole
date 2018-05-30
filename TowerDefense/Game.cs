39598f6f295120164d444e392d214b0b455c51f9
Stephanie Spears <Stephanie.Spears1@gmail.com>
2018-05-29 21:54:29 -0700

    add exceptions class

--- TowerDefense/Game.cs
+++ TowerDefense/Game.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace TowerDefense
 {
-    class Game
+    internal class Game
     {
+        public static void Main()
+        {
+            Map map = new Map(8, 5);
+
+            try
+            {
+                Path path = new Path(
+                    new[]
+                    {
+                        new MapLocations(0,2, map),
+                    }
+                    );
+            }
+        }
     }
 }
---
+++ TowerDefense/TowerDefenseException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TowerDefense
+{
+    internal class TowerDefenseException : System.Exception
+    {
+        public TowerDefenseException()
+        {
+        }
+
+        public TowerDefenseException(string message) : base(message)
+        {
+        }
+    }
+
+    internal class OutOfBoundsException : TowerDefenseException
+    {
+        public OutOfBoundsException()
+        {
+        }
+
+        public OutOfBoundsException(string message) : base(message)
+        {
+        }
+    }
+}
